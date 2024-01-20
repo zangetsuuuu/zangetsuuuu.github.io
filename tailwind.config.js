5c970ba75584d1bf9a3e085d5b51c48d5b0b85a2
Rafif Athallah <rafifathallah99@gmail.com>
2024-01-21 00:02:02 +0700

    update version (unfinished)

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#182139',
+        secondary: '#8C9ECF',
+        third: '#64748b',
+        fourth: '#A1A1AA',
+        white: '#F1F3F9',
+      }
+    },
+  },
+  plugins: [],
+}
+
